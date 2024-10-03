5a2fa4b70951857289c65b6a3e4256653b6e48ce
Ana Julia Menegasso <130059630+AnaJuliaMC@users.noreply.github.com>
2024-10-03 11:57:57 -0300

    .

---
+++ src/teclado.ts
@@ -0,0 +1,21 @@
+import { Controle, Cpu, Digito, Operação, Teclado } from "./calculadora";
+
+export default class TecladoA2 implements Teclado{
+    digiteDigito(digito: Digito): void {
+        throw new Error("Method not implemented.");
+    }
+    digiteOperacao(operação: Operação): void {
+        throw new Error("Method not implemented.");
+    }
+    digiteControle(controle: Controle): void {
+        throw new Error("Method not implemented.");
+    }
+    definaCpu(cpu: Cpu): void {
+        throw new Error("Method not implemented.");
+    }
+    obtenhaCpu(): Cpu {
+        throw new Error("Method not implemented.");
+    }
+    
+}
+ 

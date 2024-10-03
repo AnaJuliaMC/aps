7210172d4963e56c8b78a05e67ecda8c3b047a5f
Ana Julia Menegasso <130059630+AnaJuliaMC@users.noreply.github.com>
2024-10-03 11:11:41 -0300

    Update calculadora.ts

--- src/calculadora.ts
+++ src/calculadora.ts
@@ -1,57 +1,65 @@
 export enum Digito {
-    ZERO = 0,
-    UM,
-    DOIS,
-    TRÊS,
-    QUATRO,
-    CINCO,
-    SEIS,
-    SETE,
-    OITO,
-    NOVE,
-  }
-  
-  export enum Operação {
-    SOMA,
-    SUBTRAÇÃO,
-    MULTIPLICAÇÃO,
-    DIVISÃO,
-    RAIZ_QUADRADA,
-    PERCENTUAL,
-  }
-  
-  export enum Controle {
-    DESATIVAÇÃO,
-    ATIVAÇÃO_LIMPEZA_ERRO,
-    MEMÓRIA_LEITURA_LIMPEZA,
-    MEMÓRIA_SOMA,
-    MEMÓRIA_SUBTRAÇÃO,
-    SEPARADOR_DECIMAL,
-  }
-  
-  export interface Tela {
-    mostre(digito: Digito): void;
-    limpe(): void;
-  }
-  
-  export interface Teclado {
-    digiteDigito(digito: Digito): void;
-    digiteOperação(operação: Operação): void;
-    digiteControle(controle: Controle): void;
+  ZERO = 0,
+  UM,
+  DOIS,
+  TRÊS,
+  QUATRO,
+  CINCO,
+  SEIS,
+  SETE,
+  OITO,
+  NOVE,
+}
 
+export enum Operação {
+  SOMA,
+  SUBTRAÇÃO,
+  MULTIPLICAÇÃO,
+  DIVISÃO,
+  RAIZ_QUADRADA,
+  PERCENTUAL,
+}
 
-  }
-  
-  export interface Cpu {
-    recebaDigito(digito: Digito): void;
-    recebaOperação(operação: Operação): void;
-    recebaControle(controle: Controle): void;
-    reinicie(): void;
-  }
-  
-  export interface Calculadora {
-    tela: Tela;
-    teclado: Teclado;
-    cpu: Cpu;
-  }
-  
+export enum Controle {
+  DESATIVAÇÃO,
+  ATIVAÇÃO_LIMPEZA_ERRO,
+  MEMÓRIA_LEITURA_LIMPEZA,
+  MEMÓRIA_SOMA,
+  MEMÓRIA_SUBTRAÇÃO,
+  SEPARADOR_DECIMAL,
+}
+
+export interface Tela {
+  mostre(digito: Digito): void;
+  limpe(): void;
+}
+
+export interface Teclado {
+  digiteDigito(digito: Digito): void;
+  digiteOperacao(operação: Operação): void;
+  digiteControle(controle: Controle): void;
+
+  definaCpu(cpu: Cpu): void;
+  obtenhaCpu(): Cpu;
+}
+
+export interface Cpu {
+  recebaDigito(digito: Digito): void;
+  recebaOperacao(operação: Operação): void;
+  recebaControle(controle: Controle): void;
+  reinicie(): void;
+
+  definaTela(tela: Tela): void;
+  obtenhaTela(): Tela;
+}
+
+export interface Calculadora {
+  definaTela(tela: Tela): void;
+  obtenhaTela(): Tela;
+
+  definaCpu(cpu: Cpu): void;
+  obtenhaCpu(): Cpu;
+
+  definaTeclado(teclado: Teclado): void;
+  obtenhaTeclado(): Teclado;
+}
